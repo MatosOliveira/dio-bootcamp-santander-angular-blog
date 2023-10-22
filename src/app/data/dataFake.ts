b38e48976650b217974b6a940c9bd7869ea019cd
MatosOliveira <matosalem.oliveira@gmail.com>
2023-10-22 03:23:40 -0300

    Deixando a page content mais dinamica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,20 @@
 export const dataFake = [
     {
-        "id": 1,
-        "photo": "aaa",
-        "title": "novas versões",
-        "desciption": "ola"
+        "id": "1",
+        "title": "Tecnologia",
+        "description": "Ola Mundo",
+        "photoCover": "https://www.honda.com.br/automoveis/sites/hab/files/2023-01/004_Design.jpg"
     },
     {
-        "id": 2,
-        "photo": "aaa",
-        "title": "novas versões",
-        "desciption": "ola"
+        "id": "2",
+         "title": "Conforto",
+        "description": "Tchau mundo",
+        "photoCover": "https://www.honda.com.br/automoveis/sites/hab/files/2023-01/004_Design.jpg"
+    },
+    {
+        "id": "3",
+         "title": "Performance",
+        "description": "Super rapido",
+        "photoCover": "https://www.honda.com.br/automoveis/sites/hab/files/2023-01/004_Design.jpg"
     }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -8,17 +9,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContentComponent implements OnInit {
 
-  photoCover:string="https://www.honda.com.br/automoveis/sites/hab/files/2023-01/004_Design.jpg"
-  contentTitle:string="Minha Noticia"
-  contentDescription="Ola Mundo"
+  photoCover:string=""
+  contentTitle:string=""
+  contentDescription=""
+  private id:string | null = "0"
 
   constructor(private route:ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id)
   }
 
+  setValuesToComponent(id:String | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+  }
 }
